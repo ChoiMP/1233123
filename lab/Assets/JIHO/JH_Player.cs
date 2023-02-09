0ec678efb5d1364322758f6ae57f9f90f8e133e0
dhksgml <wlgh7546@naver.com>
2023-02-09 16:48:47 +0900

    대포 완성~><
    ㅈㄱㄴ

--- lab/Assets/JIHO/JH_Player.cs
+++ lab/Assets/JIHO/JH_Player.cs
@@ -1,11 +1,21 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
+public enum State { Idle, Walk, Freeze, rope, Dead };
 
 public class JH_Player : MonoBehaviour
 {
     Rigidbody rb;
 
+    //public enum State { Idle, Walk, Freeze, rope, Dead };
+   [SerializeField]  private State state = State.Idle;
+    public State myState
+    {
+        get { return state; }
+        set { state = value; }
+    }
     //Move()
     [SerializeField] float moveSpeed;
 
@@ -16,72 +26,89 @@ public class JH_Player : MonoBehaviour
 
     Vector3 lookVector;
 
+    float h;
+    float v;
+
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
     }
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         Jump();
+        InputKey();
     }
 
     private void FixedUpdate()
     {
-        Move();
+        
     }
-    private void Move()
+    private void Move(float h, float v=0)
     {
-        float h = Input.GetAxis("Horizontal");        // 가로축
-        float v = Input.GetAxis("Vertical");          // 세로축
 
-        if(h!=0||v!=0)
-        {
-            lookVector = new Vector3(h,v);
-        }
-
+      
         // Point 2.
-        transform.position += new Vector3(h, 0, 0) * moveSpeed * Time.deltaTime;
+        transform.position += new Vector3(h, v, 0) * moveSpeed * Time.fixedDeltaTime;
     }
 
     private void Jump()
     {
 
 
-         int layerMask = 1 << LayerMask.NameToLayer("Ground");
-         Debug.DrawRay(transform.position, Vector3.down * JumpCheckLen, Color.red);
-         if (Physics.Raycast(transform.position, Vector3.down, JumpCheckLen, layerMask))
-         {
-             print("점프 가능");
-             if (Input.GetKeyDown(KeyCode.C))
-             {
-                 rb.AddForce(Vector3.up * jumpPower);
-             }
-
-         }
-
-        //int layerMask = 1 << LayerMask.NameToLayer("Ground");
-/*        if (Physics.BoxCast(JumpCheck.transform.position, new Vector3(1,0.2f,1), Vector3.down, out RaycastHit hit, transform.rotation, 0))
+        int layerMask = 1 << LayerMask.NameToLayer("Ground");
+        Debug.DrawRay(transform.position, Vector3.down * JumpCheckLen, Color.red);
+        if (Physics.Raycast(transform.position, Vector3.down, JumpCheckLen, layerMask))
         {
             print("점프 가능");
             if (Input.GetKeyDown(KeyCode.C))
             {
                 rb.AddForce(Vector3.up * jumpPower);
             }
-        }*/
+
+        }
+
+      
+
 
     }
 
-/*    void OnDrawGizmos()
+    void InputKey()
     {
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireCube(JumpCheck.transform.position, new Vector3(1, 0.2f, 1));
+        if (state == State.Freeze)
+        {
+            return;
+        }
 
+        h = Input.GetAxis("Horizontal");        // 가로축
+        v = Input.GetAxis("Vertical");          // 세로축
 
-    }*/
+        if (h != 0 || v != 0)
+        {
+            lookVector = new Vector3(h, v).normalized;
+        }
+
+        if (state == State.Idle || state == State.Walk)
+        {
+            Move(h);
+        }
+        else if (state == State.rope)
+        {
+            Move(0, v);
+        }
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(state == State.Freeze)
+        {
+            state = State.Idle;
+        }
+    }
 }
--- lab/Assets/JIHO/MapObject/JH_Cannon.cs
+++ lab/Assets/JIHO/MapObject/JH_Cannon.cs
@@ -14,16 +14,30 @@ public class JH_Cannon : MonoBehaviour
     [SerializeField] int turn=1;
     [SerializeField] bool SpinStop=false;
     // Start is called before the first frame update
-    void Start()
+
+    Quaternion fristRotate;
+
+
+
+    private void Start()
     {
-        
+        fristRotate = transform.rotation;
     }
 
-    // Update is called once per frame
     void Update()
     {
         Spin();
         Check();
+    }
+
+
+    void Init()
+    {
+        transform.rotation = fristRotate;
+        isEnter = false;
+        turn = 1;
+        SpinStop = false;
+        GetComponent<CapsuleCollider>().isTrigger = false;
     }
 
     void Spin()
@@ -32,32 +46,48 @@ public class JH_Cannon : MonoBehaviour
 
         if (isEnter == true)
         {
-            if ((int)transform.eulerAngles.z%90 == 0)
+            if(spinSpeed>0)
             {
-                turn *= -1;
+                if (transform.GetChild(0).transform.position.x <= transform.position.x || transform.GetChild(0).transform.position.y <= transform.position.y)
+                {
+                    Debug.Log(transform.rotation);
+                    turn *= -1;
+                }
             }
+            else
+            {
+
+                if (transform.GetChild(0).transform.position.x >= transform.position.x || transform.GetChild(0).transform.position.y <= transform.position.y)
+                {
+                    Debug.Log(transform.rotation);
+                    turn *= -1;
+                }
+            }
+           
 
             if(SpinStop==false)
             {
-                transform.Rotate(new Vector3(0, 0, turn * spinSpeed) * Time.deltaTime);
-
+                transform.Rotate(new Vector3(0, 0, turn * spinSpeed));
             }
 
             if (Input.GetKeyDown(KeyCode.X))
             {
                 SpinStop = true;
-               
+                Fire();
             }
 
-            if(SpinStop==true)
-            {
-                //print(new Vector3(Mathf.Cos(transform.eulerAngles.z), Mathf.Cos(transform.eulerAngles.z), 0)+ "transform.eulerAngles.z" + transform.eulerAngles.z);
-                user.transform.Translate(new Vector3(( Mathf.Cos(90 + transform.eulerAngles.z))/180, -Mathf.Cos(transform.eulerAngles.z) / 180, 0) * launchPower * Time.deltaTime);
-               // user.GetComponent<Rigidbody>().AddForce()
-            }
         }
     }
 
+    void Fire()
+    {
+        user.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        user.GetComponent<Rigidbody>().useGravity = true;
+        Vector3 vec = transform.GetChild(0).transform.position - transform.position;
+        user.GetComponent<Rigidbody>().AddForce(new Vector3(vec.x, vec.y, 0) * launchPower);
+
+         Invoke("Init",0.2f);
+    }
    void Check()
     {
         int layerMask = 1 << LayerMask.NameToLayer("Player");
@@ -68,23 +98,19 @@ public class JH_Cannon : MonoBehaviour
             print("상호작용 누르면 대포 탈수 있음");
             if (Input.GetKeyDown(KeyCode.X))
             {
-                print("대포 탑승");
                 user = hit.transform.gameObject;
                 isEnter = true;
                 transform.GetComponent<CapsuleCollider>().isTrigger = true;
+                user.GetComponent<Rigidbody>().useGravity = false;
                 user.transform.position = transform.position;
+                user.GetComponent<Rigidbody>().velocity = Vector3.zero;
+                user.GetComponent<JH_Player>().myState = State.Freeze;
             }
 
         }
+
+
     }
 
-    private void OnCollisionStay(Collision collision)
-    {
-        if (collision.transform.CompareTag("Player") && isEnter ==false)
-        {
-           
-
-        }
-    }
 
 }
